991c665045c5d6654a20bd3f9a4ed261b0e2d19c
HV9019 <57114727+HV9019@users.noreply.github.com>
2026-08-30 22:08:24 +0200

    feat(m328): load registration review polish

--- js/m327-r1-guest-contact-polish.js
+++ js/m327-r1-guest-contact-polish.js
@@ -1,3 +1,5 @@
 import { setupM327AcceptancePolish } from "./m327-r1-acceptance-polish.js?v=20260829-m327-r1-acceptance1";
+import { setupM328PublicRegistrationFlow } from "./m328-r2-public-registration-flow.js?v=20260830-m328-r2-public-registration1";
 
 setupM327AcceptancePolish();
+setupM328PublicRegistrationFlow();
