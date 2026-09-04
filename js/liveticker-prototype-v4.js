00fb02ce0e0e4af699ce160bcde618dff0e78df7
HV9019 <57114727+HV9019@users.noreply.github.com>
2026-09-04 23:14:24 +0200

    fix(liveticker): refresh BEV support module

--- js/liveticker-prototype-v4.js
+++ js/liveticker-prototype-v4.js
@@ -1,4 +1,4 @@
 export * from "./liveticker-engine-v4.js";
-export { BEV_PENALTY_DURATIONS as PENALTY_DURATIONS, BEV_PENALTY_REASONS as PENALTY_REASONS } from "./liveticker-v5-support.js";
+export { BEV_PENALTY_DURATIONS as PENALTY_DURATIONS, BEV_PENALTY_REASONS as PENALTY_REASONS } from "./liveticker-v5-support.js?v=20260904-25";
 import "./liveticker-engine-v4.js";
-import "./liveticker-v5-support.js";
+import "./liveticker-v5-support.js?v=20260904-25";
