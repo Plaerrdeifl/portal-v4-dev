bc90061125c914df279c519a40093cf27995e5bd
HV9019 <57114727+HV9019@users.noreply.github.com>
2026-09-05 20:08:44 +0200

    chore(liveticker): rotate archive runtime cache key

--- js/liveticker-bootstrap.js
+++ js/liveticker-bootstrap.js
@@ -1,7 +1,7 @@
 await import("./runtime-config.js");
 
 const STORAGE_KEY = "plaerrdeifl.livetickerPrototype.v3";
-const { prepareLivetickerGameStorage } = await import("./liveticker-game-storage.js?v=20260905-calendar1");
+const { prepareLivetickerGameStorage } = await import("./liveticker-game-storage.js?v=20260905-archive1");
 
 function runtimeOpponentSource() {
   return `const runtimeGame = globalThis.PD_LIVETICKER_GAME_CONTEXT || {};
